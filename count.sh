11d89d7feb43c9531bff53cbc5b9085b0e9639dd
Thaimozhiarasan.S <thaimozhi2005@gmail.com>
2025-02-21 19:59:06 +0530

    Delete count.sh

--- count.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Count Files in Directory
-DIRECTORY="/home/thaimozhi2005/Desktop"  # Replace with the actual directory path
-
-COUNT=$(ls -lA $DIRECTORY | wc -l)
-
-echo "Number of files in $DIRECTORY: $COUNT"
